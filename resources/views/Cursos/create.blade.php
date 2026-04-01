710d7b9165a281cc365d87d9a89de44411b91794
Giovanni <giovanni.bazzo@nutriz.com.br>
2026-04-01 19:57:30 -0300

    teste

--- resources/views/Cursos/create.blade.php
+++ resources/views/Cursos/create.blade.php
@@ -6,6 +6,7 @@
     <h1>Novo Curso</h1>
 
     <form action="{{ route('cursos.store') }}" method="POST">
+        
         @include('cursos._form', ['buttonText => 'Criar curso'])
     </form>
 @endsection
